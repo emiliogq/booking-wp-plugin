b6368f81df0c11bb256452a03888f7c2cd926a4f
Emilio Garcia <github@emiliogq.com>
2019-09-08 12:07:52 +0200

    Initial structure.

---
+++ includes/class-emiliogqcom-booking-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    Emiliogqcom_Booking
+ * @subpackage Emiliogqcom_Booking/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Emiliogqcom_Booking
+ * @subpackage Emiliogqcom_Booking/includes
+ * @author     Your Name <email@example.com>
+ */
+class EmiliogcomBooking_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
